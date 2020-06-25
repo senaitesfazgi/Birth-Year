322d10a28d570a7fbcfa9b5469e9ff56bf730a54
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-06-24 19:08:41 -0600

    Initialzing the Birth year program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BirthYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompting user to enter age
+            Console.WriteLine("Enter your age");
+            //Allowing user to input their age
+            int age = int.Parse(Console.ReadLine());
+
+            //Calculating birth year
+            int birthYear = 2020 - age;
+            Console.WriteLine("Your birth year is {0}", birthYear);
+        }
+    }
+}
